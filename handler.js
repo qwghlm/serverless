cb8ebb7cc3038b68307e8f99287033ff3f2be5ba
Chris Applegate <github@qwghlm.co.uk>
2019-02-28 14:39:34 +0100

    Get basis for Twitter client working

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.tweets = async (event, context) => {
+
   return {
     statusCode: 200,
     body: JSON.stringify({
