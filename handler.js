1a2c605ab01a1e830f22418f36f9ab1f6b3dda27
Chris Applegate <github@qwghlm.co.uk>
2019-02-28 16:19:37 +0100

    Refactor

--- handler.js
+++ handler.js
@@ -1,32 +1,8 @@
 'use strict';
 
-const Twitter = require('twitter');
-
-const client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
-
-function parseTweet(tweet) {
-  if (tweet.retweeted_status) {
-    return parseTweet(tweet.retweeted_status);
-  }
-  const { full_text, entities: { media } } = tweet;
-
-  if (!media || media.length === 0) {
-    return null;
-  }
-
-  const { media_url_https } = media[0];
-
-  return { full_text, media_url_https };
-}
+const { getMedia } = require("./lib/twitter.js");
 
 module.exports.tweets = async (event, context) => {
-
-  console.log(event.queryStringParameters)
 
   if (!event.queryStringParameters || !event.queryStringParameters.screen_name) {
     return {
@@ -38,16 +14,7 @@ module.exports.tweets = async (event, context) => {
   }
 
   const { screen_name } = event.queryStringParameters;
-  const params = { screen_name, tweet_mode: "extended" };
-  const result = await client.get('statuses/user_timeline', params)
-    .then((tweets, request) => {
-      return {
-        tweets: tweets.map(parseTweet).filter(item => item !== null)
-      }
-    })
-    .catch((error) => {
-      return { error }
-    })
+  const result = await getMedia(screen_name);
 
   const statusCode = result.tweets ? 200 : 500;
   return {
