04d78ae3f706ace5653d375c6effa9e2702489fe
Chris Applegate <github@qwghlm.co.uk>
2019-02-28 15:32:00 +0100

    Update Handler to get a user's Tweets with media in the timeline

--- handler.js
+++ handler.js
@@ -1,15 +1,57 @@
 'use strict';
 
+const Twitter = require('twitter');
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+function parseTweet(tweet) {
+  if (tweet.retweeted_status) {
+    return parseTweet(tweet.retweeted_status);
+  }
+  const { full_text, entities: { media } } = tweet;
+
+  if (!media || media.length === 0) {
+    return null;
+  }
+
+  const { media_url_https } = media[0];
+
+  return { full_text, media_url_https };
+}
+
 module.exports.tweets = async (event, context) => {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      // input: event,
-    }),
-  };
+  console.log(event.queryStringParameters)
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  if (!event.queryStringParameters || !event.queryStringParameters.screen_name) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: "screen_name must be provided"
+      })
+    }
+  }
+
+  const { screen_name } = event.queryStringParameters;
+  const params = { screen_name, tweet_mode: "extended" };
+  const result = await client.get('statuses/user_timeline', params)
+    .then((tweets, request) => {
+      return {
+        tweets: tweets.map(parseTweet).filter(item => item !== null)
+      }
+    })
+    .catch((error) => {
+      return { error }
+    })
+
+  const statusCode = result.tweets ? 200 : 500;
+  return {
+    statusCode,
+    body: JSON.stringify(result),
+  };
 };
